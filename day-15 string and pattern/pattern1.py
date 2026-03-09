4b4de6f99702d1afbc12403428ded6c2f91116c0
Ravikantyadav18213 <raviyadav18213@gmail.com>
2026-03-09 10:17:05 +0000

    pattern2

--- day-15 string and pattern/pattern1.py
+++ day-15 string and pattern/pattern1.py
@@ -4,8 +4,36 @@
 # for i in range((3)):
 #     print("*",end="  ")    
 
-for i in range(3):
-    for a in range(3):
-        print("*",end=" ")
-    print()    
+# for i in range(3):
+#     for a in range(3):
+#         print("*",end=" ")
+#     print()    
  
+
+ # left-incremental triangle
+count=int(input("enter row count="))
+for i in range(count):
+    for j in range(i+1):
+        print("*",end="")
+    print()
+
+count=int(input("enter row count="))
+for i in range(count):
+    for j in range(i+1):
+        print(1,end="")
+    print()    
+
+
+count=int(input("enter row count="))
+for i in range(count):
+    for j in range(i+1):
+        print(j,end="")
+    print()
+
+
+count=int(input("enter row count="))
+n=1
+for i in range(count):
+    for j in range(i+1):
+        print(n,end="")
+    print()
