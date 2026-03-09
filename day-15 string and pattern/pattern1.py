4642bbf34caa9b51042d189bef9df8a6fb8f312c
Ravikantyadav18213 <raviyadav18213@gmail.com>
2026-03-09 09:28:32 +0000

    pattern1

---
+++ day-15 string and pattern/pattern1.py
@@ -0,0 +1,11 @@
+# for i in range(3):
+#     print("*")
+
+# for i in range((3)):
+#     print("*",end="  ")    
+
+for i in range(3):
+    for a in range(3):
+        print("*",end=" ")
+    print()    
+ 
---
+++ day-15 string and pattern/stringforloop.py
@@ -0,0 +1,62 @@
+# method-count,find replace ,join,index,split,splice
+
+#iteratte srting
+# name="itvedant"
+# for i in name:
+#     print(i)
+
+
+#reverse string
+# print("reverse string:",name[::-1])
+
+
+# newstr=""
+# for i in name:
+#     newstr=i+newstr
+
+# print(newstr)    
+
+# str=input("enter string=")
+# char=input("enter character=")
+# count=0
+# for i in str:
+#     if char==i:
+#         count+=1
+
+# print(f"{char} character count into {str} is {count}")        
+
+
+#wao to print number of occurance of entered 
+#character or word from any string
+
+# str=input("enter number=")
+# chkstr=input("entr char/word to find and show count=")
+# count=0
+# check=""
+# for i in range(len(str)):
+#     print(i,str[i])
+
+
+
+str=input("enter number=")
+# chkstr=input("entr char/word to find and show count=")
+# count=0
+# check=""
+# for i in range(len(str)):
+#     k=i
+#     for j in range(len(chkstr)):
+#         try:
+#             chkstr+=str[k]
+#             k+=1
+#         except:
+#             None
+#     if check==chkstr:
+#         count+=1
+#     check=""
+
+# if count!=0:
+#     print(f"{chkstr} found in {str} and count={count}")
+# else:
+#     print(f"{chkstr} not found in {str}")        
+        
+       
