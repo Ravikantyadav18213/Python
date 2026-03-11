af1edc2d517b976255cf2202890bed92eecb17cb
Ravikantyadav18213 <raviyadav18213@gmail.com>
2026-03-11 10:28:57 +0000

    listmethod1

--- day-15 string and pattern/librarycrud.py
+++ day-15 string and pattern/librarycrud.py
@@ -16,9 +16,28 @@ while True:
         else:
             print("Available book are:",books)
     elif choice==3:
-        pass
+        if len(books)>0:
+           old_book_title=input("enter book title to update:").capitalize()
+           if old_book_title in books:
+                new_title=input("Enter book new title to update:").capitalize()
+                books[books.index(old_book_title)]=new_title . capitalize()
+                print(f"{old_book_title}Book title updated succesfully!!")
+           else:
+               print(f"{old_book_title}Book title not found try again!!")
+        else:
+            print("no book available so first add the books!!")
+
     elif choice==4:
-        pass
+        if len(books)>0:
+            old_book_title=input("enter book title to remove:").capitalize()
+            if old_book_title in books:
+                books.remove(old_book_title.capitalize())
+                print(f"{old_book_title}Book removed successfully")
+            else:
+                print(f"{old_book_title}Book title not found try again!!")
+        else:
+            print("no book available so first add the books!!")
+
     elif choice==5:
         print("Thank for using our services")
         break
---
+++ day-15 string and pattern/listmethod.py
@@ -0,0 +1,15 @@
+fruits=["apple","graps","banana","kiwi","watermelon"]
+print(fruits)
+fruits.reverse()
+print(fruits)
+fruits.sort()
+print(fruits)
+
+price=[200,300,400,500]
+fruits.extend(price)
+print(fruits)
+print(fruits.count("apple"))
+x=[1,2,3]
+print(x)
+x=fruits.copy()
+print(x)
