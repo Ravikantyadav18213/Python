1e0056992a1160831aefc808f927334c631d3cf4
Ravikantyadav18213 <raviyadav18213@gmail.com>
2026-03-12 08:18:37 +0000

    rer

---
+++ day-15 string and pattern/listcomprehension.py
@@ -0,0 +1,6 @@
+#methods-index(),append(),insert9(),pop(),remove(),reverse(),short(),copy(),extend(),clear(),cout()
+
+#list comprehension
+#syntax [output for iterable_variable in sequence_object if condation]
+#print 1to 10
+print([i for i in range(1,11)])
